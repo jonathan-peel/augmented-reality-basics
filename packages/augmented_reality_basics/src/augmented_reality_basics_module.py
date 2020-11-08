cf7d6aa959dd4b615c3865bd6f4eba267b9f771c
jonathan-peel <jonathan.n.peel@gmail.com>
2020-11-08 16:42:19 +0100

    still working for hud.yaml

--- packages/augmented_reality_basics/src/augmented_reality_basics_module.py
+++ packages/augmented_reality_basics/src/augmented_reality_basics_module.py
@@ -14,6 +14,9 @@ class Augmenter():
 
 
     def ground2pixel(self):
+        """ Take in points in the 'axle' reference frame, and run them in the 
+            camera reference frame.
+        """
         pass
 
 
@@ -22,6 +25,7 @@ class Augmenter():
             Inputs: image_data from the subscribed topic
         """
 
+        # Extract the image from the subscriber into a numpy array
         np_arr = np.fromstring(image_data.data, np.uint8)
         # image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR) # OpenCV < 3
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
@@ -29,23 +33,31 @@ class Augmenter():
         # draw on the image
         image_width = image_np.shape[1]
         image_height = image_np.shape[0]
-        segments = map_dict['segments'] # a list of segments dicts
         points = map_dict['points'] # dict of points
+        rospy.loginfo(f'Points: {points}')
+        segments = map_dict['segments'] # a list of segments dicts
+        rospy.loginfo(f'Segments: {segments}')
 
+        # translate all points into image frame
+        
+
+        # deconstruct map_dict into pt_x and pt_y for each segment and plot it
         for segment in segments:
-            # deconstruct map_dict into pt_x and pt_y
-            segment_points_names = segment['points'] # eg [TL, TR]
+            segment_points_names = segment['points'] # eg ['TL', 'TR']
+
             segment_points_coords = []
             segment_points_coords.append(points[segment_points_names[0]][1]) # eg [0, 0]
             segment_points_coords.append(points[segment_points_names[1]][1])
             # segment_points_coords now eg [[0, 0], [0, 1]]
             pt_x = [segment_points_coords[0][0], segment_points_coords[1][0]]
             pt_y = [segment_points_coords[0][1], segment_points_coords[1][1]]
+            print(f'pt_x: {pt_x}')
+            print(f'pt_y: {pt_y}')
             # pt_x is eg [0, 0]
             segment_color = segment['color'] # eg 'red'
 
-            pt_x = [i * image_width for i in pt_x] # just so you can actually see it!
-            pt_y = [i * image_height for i in pt_y] # just so you can actually see it!
+            # pt_x = [i * image_width for i in pt_x] # just so you can actually see it!
+            # pt_y = [i * image_height for i in pt_y] # just so you can actually see it!
 
             image_np = self.draw_segment(image_np, pt_x, pt_y, segment_color)
 
